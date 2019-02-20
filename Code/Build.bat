28a1fe8b24d43f21e00b6b0772e785d2c1a3e763
Doyle <doylet@protonmail.com>
2019-02-21 00:22:21 +1100

    Add DqnReflect, deprecate old library
    They're horrible. I'm able to write better versions now.

---
+++ Code/Build.bat
@@ -0,0 +1,16 @@
+@echo OFF
+
+if not exist ..\Bin mkdir ..\Bin
+pushd ..\Bin
+
+REM MT     Static CRT
+REM EHa-   Disable exception handling
+REM GR-    Disable C RTTI
+REM O2     Optimisation Level 2
+REM Oi     Use CPU Intrinsics
+REM Z7     Combine multi-debug files to one debug file
+REM wd4201 Nonstandard extension used: nameless struct/union
+REM Tp     Treat header file as CPP source file
+cl /MT /EHa /GR- /O2 /Oi /Z7 /W4 /WX /D DQN_REFLECT_IMPLEMENTATION /Tp ../Code/DqnReflect.h /link /nologo
+
+popd
---
+++ Data/DqnReflect_TestData.h
@@ -0,0 +1,6 @@
+DQN_REFLECT enum struct OpenGLShader
+{
+    Invalid,
+    Rect DQN_REFLECT_META(VertexShaderFilePath = "Rect.vert", FragmentShaderFilePath = "Rect.frag"),
+    Text DQN_REFLECT_META(VertexShaderFilePath = "Text.vert",   FragmentShaderFilePath = "Text.frag"),
+};
